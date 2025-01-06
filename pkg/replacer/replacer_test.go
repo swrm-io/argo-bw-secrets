ca92bfa79410e0df2e8307416dc87929f0dca4cb
Richard Cox <code@swrm.io>
2025-01-05 19:25:23 -0700

    [Fix] tests for new SDK version

--- pkg/replacer/replacer_test.go
+++ pkg/replacer/replacer_test.go
@@ -2,6 +2,7 @@ package replacer
 
 import (
 	"testing"
+	"time"
 
 	bitwarden "github.com/bitwarden/sdk-go"
 	"github.com/stretchr/testify/assert"
@@ -13,26 +14,34 @@ func (t TestLookup) Get(secretID string) (*bitwarden.SecretResponse, error) {
 	switch secretID {
 	case "ce398fa2-5665-11ef-8916-97605d6da25b":
 		projectID := "ddb13dae-5665-11ef-8583-f73233caa8df"
+		revDate, err := time.Parse("2006-01-02T15:04:05.000Z", "2022-11-17T15:55:18.005Z")
+		if err != nil {
+			return nil, err
+		}
 		return &bitwarden.SecretResponse{
-			CreationDate:   "2022-11-17T15:55:18.005669100Z",
+			CreationDate:   revDate,
 			ID:             secretID,
 			Key:            "Human Readable Key",
 			Note:           "",
 			OrganizationID: "d4105690-5665-11ef-a058-c713a9374bb0",
 			ProjectID:      &projectID,
-			RevisionDate:   "2022-11-17T15:55:18.005669100Z",
+			RevisionDate:   revDate,
 			Value:          "my_secret_password",
 		}, nil
 	case "98b6c8ee-5666-11ef-ac37-8742ac5fc78f":
 		projectID := "ddb13dae-5665-11ef-8583-f73233caa8df"
+		revDate, err := time.Parse("2006-01-02T15:04:05.000Z", "2019-05-11T15:55:18.005Z")
+		if err != nil {
+			return nil, err
+		}
 		return &bitwarden.SecretResponse{
-			CreationDate:   "2019-05-11T15:55:18.005669100Z",
+			CreationDate:   revDate,
 			ID:             secretID,
 			Key:            "Other Key",
 			Note:           "",
 			OrganizationID: "d4105690-5665-11ef-a058-c713a9374bb0",
 			ProjectID:      &projectID,
-			RevisionDate:   "2019-05-11T15:55:18.005669100Z",
+			RevisionDate:   revDate,
 			Value:          "my_other_secret",
 		}, nil
 	default:
